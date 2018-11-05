ca44cb9fc1f708dd69ef43ec36a971734e0d1ecd
vidavenel <vincent_mil@hotmail.fr>
2018-11-05 13:27:02 +0100

    fix model & migration

--- src/migrations/2018_11_05_132000_create_payzen_paiements_table.php
+++ src/migrations/2018_11_05_132000_create_payzen_paiements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaiementsTable extends Migration
+class CreatePayzenPaiementsTable extends Migration
 {
     /**
      * Run the migrations.
